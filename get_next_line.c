bdaad01c5b92cce9d7b7e9dc3467377c58a6b938
Yurii Butkov <ybutkov@1-D-16.42heilbronn.de>
2025-07-29 12:37:33 +0200

    1st working version

---
+++ get_next_line.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 17:40:04 by ybutkov           #+#    #+#             */
+/*   Updated: 2025/07/29 12:25:04 by ybutkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+static char	*fill_str_from_buffer(char *cur_str, int *total_len, char *buffer);
+static char	*read_from_fd(int fd, char *res, int *cur_str_size, char *buffer);
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[FD_MAX_SIZE][BUFFER_SIZE];
+	ssize_t		read_bytes;
+	int			total_len;
+	char		*res;
+
+	if ((fd < 0 || fd >= FD_MAX_SIZE) && read(fd, 0, 0) < 0)
+		return (NULL);
+	res = NULL;
+	total_len = 0;
+	if (buffer[fd][0])
+		res = fill_str_from_buffer(NULL, &total_len, buffer[fd]);
+	if (buffer[fd][0] != '\0' && res == NULL)
+		return (NULL);
+	if (res && res[total_len - 1] == '\n')
+		return (res[total_len] = '\0', res);
+	read_bytes = read(fd, buffer[fd], BUFFER_SIZE);
+	if (read_bytes < 0)
+	{
+		if (res)
+			free(res);
+		return (NULL);
+	}
+	res = read_from_fd(fd, res, &total_len, buffer[fd]);
+	return (res);
+}
+
+static char	*read_from_fd(int fd, char *res, int *cur_str_size, char *buffer)
+{
+	ssize_t		read_bytes;
+
+	read_bytes = 1;
+	while (read_bytes > 0)
+	{
+		res = fill_str_from_buffer(res, cur_str_size, buffer);
+		if (!res)
+			return (NULL);
+		if (res[*cur_str_size - 1] == '\n')
+			break ;
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes < 0)
+		{
+			if (res)
+				free(res);
+			return (NULL);
+		}
+	}
+	if (res)
+		res[*cur_str_size] = '\0';
+	return (res);
+}
+
+static char	*fill_str_from_buffer(char *cur_str, int *total_len, char *buffer)
+{
+	int		len;
+	char	*res;
+
+	len = 0;
+	res = NULL;
+	while (buffer[len] && len < BUFFER_SIZE && buffer[len] != '\n')
+		len++;
+	if (buffer[len] == '\n')
+		len += 1;
+	if (len == 0)
+		return (cur_str);
+	res = malloc((*total_len + len + 1) * sizeof(char));
+	if (res == NULL)
+		return (free(cur_str), NULL);
+	if (cur_str)
+	{
+		ft_memmove(res, cur_str, *total_len);
+		free(cur_str);
+	}
+	ft_memmove(res + *total_len, buffer, len);
+	ft_memmove(buffer, buffer + len, BUFFER_SIZE - len);
+	ft_memset(buffer + BUFFER_SIZE - len, '\0', len);
+	*total_len += len;
+	return (res);
+}
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 17:40:48 by ybutkov           #+#    #+#             */
+/*   Updated: 2025/07/29 12:31:06 by ybutkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 64
+# endif
+
+# ifndef FD_MAX_SIZE
+#  define FD_MAX_SIZE 1000
+# endif
+
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
